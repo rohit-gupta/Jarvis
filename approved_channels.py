f548dbeade60754d7c0f8019fa6e5d62720ff26c
Rohit Gupta <rohitgupta.hpf@gmail.com>
2016-09-21 18:29:25 +0530

    Adding code to search YouTube using API

---
+++ approved_channels.py
@@ -0,0 +1,33 @@
+id_to_channel = {}
+
+id_to_channel['UC2-BeLxzUBSs0uSrmzWhJuQ'] = "20th Century Fox"
+id_to_channel['UCor9rW6PgxSQ9vUPWQdnaYQ'] = "FoxSearchlight"
+
+id_to_channel['UCz97F7dMxBNOfGYu3rx8aCw'] = "Sony Pictues"
+id_to_channel['UCFqyJFbsV-uEcosvNhg0PaQ'] = "Sony Pictures India"
+
+id_to_channel['UCjmJDM5pRKbUlVIzDYYWb6g'] = "Warner Bros"
+id_to_channel['UCbxjDfYwfF8RjYBp5htJ38Q'] = "Warner Bros UK"
+
+id_to_channel['UC9YHyj7QSkkSg2pjQ7M8Khg'] = "Paramount Movies"
+id_to_channel['UCpVqzaoo6_5dKbmgFGK1M_Q'] = "Paramount Movies NL"
+
+id_to_channel['UCq0OueAsdxH6b8nyAspwViw'] = "Universal Pictures"
+id_to_channel['UCIHg3K4EaY-ziWc5CyzWAzQ'] = "UniversalMovies"
+
+id_to_channel['UCZGYJFUizSax-yElQaFDp5Q'] = "Star Wars"
+id_to_channel['UC_IRYSp4auq7hKLvziWVH6w'] = "Disney-Pixar"
+id_to_channel['UCV3uIARCxZY6lWbeVVRZsKg'] = "DisneyMoviesOnDemand"
+id_to_channel['UCuaFvcY4MhZY3U43mMt1dYQ'] = "Disney Movie Trailers"
+
+id_to_channel['UCJ6nMHaJPZvsJ-HmUmj1SeA'] = "Lionsgate Movies"
+
+
+id_to_channel['UCi8e0iOVk1fEOogdfu4YgfA'] = "Movie Clips Trailers"
+id_to_channel['UCTCjFFoX1un-j7ni4B6HJ3Q'] = "Movieclips Trailer Vault"
+
+id_to_channel['UCw7TdYd1d873DtdjkdKStUg'] = "White Yak Trailer"
+
+id_to_channel['UCDoLDRl0RbBWPIwR2xjjneg'] = "Video Detective"
+
+ids = id_to_channel.keys()
