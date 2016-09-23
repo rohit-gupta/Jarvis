511597e2ff75fb81d99c23554f0914b207afb3f6
Rohit Gupta <rohitgupta.hpf@gmail.com>
2016-09-24 02:27:35 +0530

    Factor out API Key

---
+++ youtube_api.py
@@ -0,0 +1,7 @@
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = "AIzaSyAB6p3VGqOsDBL_q-c4rNDRjTPMfMBPsDA"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
--- ytsearch_api.py
+++ ytsearch_api.py
@@ -6,15 +6,7 @@ from oauth2client.tools import argparser
 
 import json
 
-
-
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyAB6p3VGqOsDBL_q-c4rNDRjTPMfMBPsDA"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+from youtube_api import *
 
 def youtube_search(options):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
